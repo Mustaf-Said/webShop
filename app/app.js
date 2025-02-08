175af81c9130465ed0817ba087da14b85a80ec0f
Musse <86mussai@skola.boras.se>
2025-02-08 11:44:28 +0100

    css update

--- app/app.js
+++ app/app.js
@@ -54,3 +54,89 @@ function renderPayPalButton() {
 
 // Kalla på renderPayPalButton när sidan laddas
 document.addEventListener("DOMContentLoaded", renderPayPalButton);
+
+/* Photos Slide Gallary */
+let slideIndex = 0;
+const images = [
+  "/img/vitamin/man/4.jpeg",
+  "/img/vitamin/childeren/2.jpeg",
+  "/img/vitamin/bregnet/3.jpeg",
+  "/img/vitamin/women/1.jpeg",
+  "/img/vitamin/oldMan/3.jpeg",
+  "/img/vitamin/oldWomen/4.jpeg",
+];
+
+// Shuffle images array randomly
+function shuffleImages() {
+  for (let i = images.length - 1; i > 0; i--) {
+    const j = Math.floor(Math.random() * (i + 1));
+    [images[i], images[j]] = [images[j], images[i]];
+  }
+}
+
+// Display the current slide
+function showSlide(index) {
+  const slide = document.getElementById("slide");
+  slide.src = images[index];
+}
+
+// Go to the next slide
+function nextSlide() {
+  slideIndex = (slideIndex + 1) % images.length;
+  showSlide(slideIndex);
+}
+
+// Start the automatic slideshow with random order
+function startAutoSlideshow() {
+  shuffleImages(); // Shuffle images before starting
+  showSlide(slideIndex); // Show the initial slide
+
+  // Change slides every 3 seconds
+  setInterval(() => {
+    nextSlide();
+  }, 3000);
+}
+
+// Start the slideshow on page load
+window.onload = startAutoSlideshow;
+
+/* Home Main photos galary med prev & next button */
+let currentImageIndex = 0;
+const img = [
+  "/img/vitamin/man/4.jpeg",
+  "/img/vitamin/childeren/2.jpeg",
+  "/img/vitamin/bregnet/3.jpeg",
+  "/img/vitamin/women/1.jpeg",
+  "/img/vitamin/oldMan/3.jpeg",
+  "/img/vitamin/oldWomen/1.jpeg",
+]; // Lägg till dina bildfiler här
+
+function changeSlide(direction) {
+  currentImageIndex += direction;
+
+  // Håller index inom gränserna av bildarrayen
+  if (currentImageIndex < 0) {
+    currentImageIndex = img.length - 1;
+  } else if (currentImageIndex >= img.length) {
+    currentImageIndex = 0;
+  }
+
+  // Ändrar bildkälla
+  document.getElementById("galleryImage").src = img[currentImageIndex];
+}
+
+/* home txt toggle -------------------------------*/
+
+const hideFunction1 = (e) => {
+  const pTag = document.getElementById("p1");
+  pTag.classList.toggle("hidden");
+  console.log(e.target);
+};
+const hideFunction2 = () => {
+  const pTag = document.getElementById("p2");
+  pTag.classList.toggle("hidden");
+};
+const hideFunction3 = () => {
+  const pTag = document.getElementById("p3");
+  pTag.classList.toggle("hidden");
+};
